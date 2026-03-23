a218f6220db5bfe1dece1156a9e2a4f128f203b5
Gilbert Appiah <gilbertappiah@icloud.com>
2026-03-23 12:18:18 -0400

    Fix fake credentials in demo files for push protection and secret scanning tests

--- demo/push-protection-test.sh
+++ demo/push-protection-test.sh
@@ -5,7 +5,7 @@
 # ============================================================
 
 # This fake AWS key will trigger GitHub push protection
-export AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLEB"
+export AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLEBN"
 export AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEYGHHG"
 
 echo "Connecting to database..."
